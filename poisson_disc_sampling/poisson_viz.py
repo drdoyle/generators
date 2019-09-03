04c4f0c0bcfc06db8476d7c51db679faf57a8449
drdoyle <42181067+drdoyle@users.noreply.github.com>
2019-09-03 09:41:29 -0400

    Moved distribution generation to self.generate() method.
    Saves time on initial creation, allows us to regenerate the distribution without creating an entirely new object

--- poisson_disc_sampling/poisson_viz.py
+++ poisson_disc_sampling/poisson_viz.py
@@ -5,22 +5,31 @@ from visualize import SampleDrawing
 
 class PoissonDist(SampleDrawing):
 
-    def __init__(self, width, height, n, radius, max_sample=30):
+    def __init__(self, width, height):
         super().__init__(width, height)
+
+        self.n = "Sample not yet generated."
+        self.radius = "Sample not yet generated."
+        self.half_rad = "Sample not yet generated."
+        self.max_sample = "Sample not yet generated."
+        self.points = "Sample not yet generated."
+
+    def generate(self, n, radius, max_sample=30):
         self.n = n
         self.radius = radius
+        self.half_rad = int(self.radius / 2)
         self.max_sample = max_sample
-        self.points = pd_samp.generate_math(n=self.n, radius=radius, width=width,
-                                            height=height, k=self.max_sample)
-        self.half_rad = int(self.radius/2)
+        self.points = pd_samp.generate_poisson_sampling(num_points=self.n, radius=self.radius, width=self.width,
+                                                        height=self.height, max_sample=self.max_sample)
 
-        pygame.display.set_caption("Poisson Disc Sampling")
 
     def visualize(self):
         """
         Draws the points to the window.
         :return: None
         """
+
+        pygame.display.set_caption("Poisson Disc Sampling")
         for pt in self.points:
             pt = (int(pt[0]), int(pt[1]))
             pygame.draw.circle(self.win, (255, 255, 255), pt, max(1, int(self.half_rad/100)), 0)
@@ -29,5 +38,6 @@ class PoissonDist(SampleDrawing):
 
 if __name__ == "__main__":
     import math
-    dist = PoissonDist(width=800, height=600, n=10000, radius=5*math.sqrt(2))
+    dist = PoissonDist(width=800, height=600)
+    dist.generate(n=10000, radius=5*math.sqrt(2))
     dist.draw()
